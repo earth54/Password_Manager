de74c36c274e45e16500d592e340526a2c67e3d2
Richard DeGeus <crimson_eagle_26@hotmail.com>
2023-11-11 05:15:13 +0000

    optimized code

--- utility/utility.py
+++ utility/utility.py
@@ -5,218 +5,281 @@
 from pymongo import MongoClient
 from pymongo import errors
 from pymongo.server_api import ServerApi
+from pymongo.errors import OperationFailure
 from typing import Any, Dict
 
-uri = "mongodb+srv://cluster1.cjufb6h.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
+uri = 'mongodb+srv://cluster1.cjufb6h.mongodb.net/?authSource=%24external'  \
+        '&authMechanism=MONGODB-X509&retryWrites=true&w=majority'
+
 path_to_certificate = 'pm_cert.pem'
 
 
 def create_connection() -> None:
-    
-    # Create a new client and connect to the server
-    client = MongoClient(uri,tls=True,
-            tlsCertificateKeyFile=path_to_certificate,
-            server_api=ServerApi('1'))
-    
+
+    """Creates connection to MongoDB database
+
+    Raises:
+        ex: Raises an error if found
+    """
+
+    client = MongoClient(uri, tls=True,
+                         tlsCertificateKeyFile=path_to_certificate,
+                         server_api=ServerApi('1'))
+
     try:
         db = client['testDB']
         collection = db['testCol']
         doc_count = collection.count_documents({})
         print(doc_count)  # Should print 0 as the testDB doesn't exist
-    except errors.ConnectionFailure as err:
-        raise err
-    
+    except errors.ConnectionFailure as ex:
+        print(ex)
+        raise ex
+
 
 def create_collection(database_name: str, collection_name: str) -> None:
-    
+
+    """Creates a collection
+
+    Args:
+        database_name (str): Name of MongoDB database
+        collection_name (str): Name of MongoDB collection
+
+    Raises:
+        ex: Raises an error if found
+    """
+
     client = MongoClient(uri, tls=True,
-            tlsCertificateKeyFile=path_to_certificate,
-            server_api=ServerApi('1'))
+                         tlsCertificateKeyFile=path_to_certificate,
+                         server_api=ServerApi('1'))
 
     try:
         db = client[database_name]
         db.create_collection(collection_name)
-    except Exception as err:
-        raise err
-    
-    
-def insert_entry(database_name: str, collection_name: str, entry: Dict[str, Any]) -> None:
-    
+    except OperationFailure as ex:
+        print(ex)
+        raise ex
+
+
+def insert_entry(database_name: str,
+                 collection_name: str, entry: list[str, Any]) -> None:
+
+    """Inserts one {key: value} pair into collection
+
+    Args:
+        database_name (str): Name of MongoDB database
+        collection_name (str): Name of MongoDB collection
+        entry (list[str, Any]): A single {key: value} listing to insert into
+            the collection
+
+    Raises:
+        ex: Raises an error if found
+    """
+
     client = MongoClient(uri, tls=True,
-        tlsCertificateKeyFile=path_to_certificate,
-        server_api=ServerApi('1'))
-    
+                         tlsCertificateKeyFile=path_to_certificate,
+                         server_api=ServerApi('1'))
+
     try:
         db = client[database_name]
         collection = db[collection_name]
-        collection.insert_one(entry)
-    except Exception as err:
-        raise err
-    
-    
-def insert_entries(database_name: str, collection_name: str, entries: Dict[str, Any]) -> None:
-    
+        result = collection.insert_one(entry)
+        print(result.inserted_id)  # prints inserted _id
+    except OperationFailure as ex:
+        print(ex)
+        raise ex
+
+
+def insert_entries(database_name: str, collection_name: str,
+                   entries: list[Any, Any]) -> None:
+
+    """Inserts mutltiple {key: value} entries into collection as long as they
+        are in a list
+
+    Args:
+        database_name (str): Name of MongoDB database
+        collection_name (str): Name of MongoDB collection
+        entries (list[Any, Any]): List of the {key: value} pairs to insert
+            into the collection
+
+    Raises:
+        ex: Raises an error if found
+    """
+
     client = MongoClient(uri, tls=True,
-        tlsCertificateKeyFile=path_to_certificate,
-        server_api=ServerApi('1'))
-    
+                         tlsCertificateKeyFile=path_to_certificate,
+                         server_api=ServerApi('1'))
+
     try:
         db = client[database_name]
         collection = db[collection_name]
-        collection.insert_many(entries)
-    except Exception as err:
-        raise err    
-    
-    
-def find_entries(database_name: str, collection_name: str, entries: Dict[str, Any] | None = None) -> None:
-    
+        result = collection.insert_many(entries)
+        print(result.inserted_ids)  # prints inserted _ids
+    except OperationFailure as ex:
+        print(ex)
+        raise ex
+
+
+def find_entries(database_name: str, collection_name: str,
+                 entries: Dict[str, Any] | None = None) -> Any:
+
+    """Finds {key: value} listings in a collection
+
+    Args:
+        database_name (str): Name of MongoDB database
+        collection_name (str): Name of MongoDB collection
+        entries (Dict[str, Any] | None, optional): If variable name "entries"
+        is not passed in, it will find and return all listings in collection.
+        If variable name "entries" is passed in, it will search for matching
+        key in {key: value} and return all matching listings in collection.
+        Variable name entries defaults to None.
+
+    Raises:
+        ex: Raises an error if found
+
+    Returns:
+        Any: Returns list of collection dictionary entries
+    """
+
     client = MongoClient(uri, tls=True,
-        tlsCertificateKeyFile=path_to_certificate,
-        server_api=ServerApi('1'))
-    
+                         tlsCertificateKeyFile=path_to_certificate,
+                         server_api=ServerApi('1'))
+
     try:
         if entries is None:
             db = client[database_name]
             collection = db[collection_name]
             cursor = collection.find()
+            documents = list(cursor)
+            return documents
+
         else:
+            db = client[database_name]
+            collection = db[collection_name]
             cursor = collection.find(entries)
-        documents = list(cursor)
-        return documents
-    except Exception as err:
-        raise err    
-    
-    
-def update_entry(database_name: str, collection_name: str, old_data: Dict[str, Any], new_data: Dict[str, Any] ) -> None:
-    
+            documents = list(cursor)
+            return documents
+    except OperationFailure as ex:
+        print(ex)
+        raise ex
+
+
+def update_entry(database_name: str, collection_name: str,
+                 old_data: Dict[str, Any], new_data: Dict[str, Any]) -> None:
+
+    """Finds the first matching key of {key: value} and updates the value
+
+    Args:
+        database_name (str): Name of MongoDB database
+        collection_name (str): Name of MongoDB collection
+        old_data (Dict[str, Any]): The {key: value} that needs to be removed
+            from the first matching entry
+        new_data (Dict[str, Any]): the {key: value} that needs to take the
+            place of the first matching {key:value} in the collection
+
+    Raises:
+        ex: Raises an error if found
+    """
+
     client = MongoClient(uri, tls=True,
-        tlsCertificateKeyFile=path_to_certificate,
-        server_api=ServerApi('1'))
-    
+                         tlsCertificateKeyFile=path_to_certificate,
+                         server_api=ServerApi('1'))
+
     try:
         db = client[database_name]
         collection = db[collection_name]
         updated_data = collection.update_one(old_data, {"$set": new_data})
-        return updated_data.modified_count
-    except Exception as err:
-        raise err    
-    
+        # prints the number of {key: value} affected
+        print(updated_data.modified_count)
+    except OperationFailure as ex:
+        print(ex)
+        raise ex
 
-def update_entries(database_name: str, collection_name: str, old_data: Dict[str, Any], new_data: Dict[str, Any] ) -> None:
-    
+
+def update_entries(database_name: str, collection_name: str,
+                   old_data: Dict[str, Any], new_data: Dict[str, Any]) -> None:
+
+    """Finds the all matching keys of {key: value} and updates the values
+
+    Args:
+        database_name (str): Name of MongoDB database
+        collection_name (str): Name of MongoDB collection
+        old_data (Dict[str, Any]): The {key: value} that needs to be removed
+            from the collection
+        new_data (Dict[str, Any]): the {key: value} that needs to take the
+            place of all removed {key:value} in the collection
+
+    Raises:
+        ex: Raises an error if found
+    """
+
     client = MongoClient(uri, tls=True,
-        tlsCertificateKeyFile=path_to_certificate,
-        server_api=ServerApi('1'))
-    
+                         tlsCertificateKeyFile=path_to_certificate,
+                         server_api=ServerApi('1'))
+
     try:
         db = client[database_name]
         collection = db[collection_name]
         updated_data = collection.update_many(old_data, {"$set": new_data})
-        # return updated_data.modified_count
-    except Exception as err:
-        raise err    
-    
+        # prints the number of {key: value} pairs affected
+        print(updated_data.modified_count)
+    except OperationFailure as ex:
+        print(ex)
+        raise ex
 
-def delete_entry(database_name: str, collection_name: str, old_data: Dict[str, Any]) -> None:
-    
+
+def delete_entry(database_name: str, collection_name: str,
+                 old_data: Dict[str, Any]) -> None:
+
+    """Deletes the first matching {key: value} filter entry
+
+    Args:
+        database_name (str): Name of MongoDB database
+        collection_name (str): Name of MongoDB collection
+        old_data (Dict[str, Any]): The {key: value} you want to delete from
+            the first matching entry in the collection
+
+    Raises:
+        ex: Raises an error if found
+    """
+
     client = MongoClient(uri, tls=True,
-        tlsCertificateKeyFile=path_to_certificate,
-        server_api=ServerApi('1'))
-    
+                         tlsCertificateKeyFile=path_to_certificate,
+                         server_api=ServerApi('1'))
+
     try:
         db = client[database_name]
         collection = db[collection_name]
         result = collection.delete_one(old_data)
-    except Exception as err:
-        raise err    
-    
-    
-def delete_entries(database_name: str, collection_name: str, old_data: Dict[str, Any]) -> None:
-    
+        print(result.deleted_count)  # prints the number of deleted entries
+    except OperationFailure as ex:
+        print(ex)
+        raise ex
+
+
+def delete_entries(database_name: str, collection_name: str,
+                   old_data: Dict[str, Any]) -> None:
+
+    """Deletes entries matching {key: value} filter
+
+    Args:
+        database_name (str): Name of MongoDB database
+        collection_name (str): Name of MongoDB collection
+        old_data (Dict[str, Any]): The {key: value} you want to delete from
+            all entries in the collection
+
+    Raises:
+        ex: Raises an error if found
+    """
+
     client = MongoClient(uri, tls=True,
-        tlsCertificateKeyFile=path_to_certificate,
-        server_api=ServerApi('1'))
-    
+                         tlsCertificateKeyFile=path_to_certificate,
+                         server_api=ServerApi('1'))
+
     try:
         db = client[database_name]
         collection = db[collection_name]
         result = collection.delete_many(old_data)
-    except Exception as err:
-        raise err   
-
-    
-
-    
-if __name__ == "__main__":
-    create_connection()
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-# import requests
-# # import json
-# from typing import Any, Dict
-
-
-
-# # let's use API provided by Internation Space Station
-# # http://api.open-notify.org/iss-now.json
-# response = requests.get('http://api.open-notify.org/iss-now.json')
-# print(response.json())
-
-
-# end_point = 'https://us-east-1.aws.data.mongodb-api.com/app/data-nisvn/endpoint/data/v1/action'
-
-# funct = 'find'
-
-# uri = "mongodb+srv://cluster0.xeszaub.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
-
-# api_key = 'LFkYS0X0JeQT15CCgs1hsuIhrW4q3QCCHFRk50QkWH8fV0spYo44tdAQwNXgMea0'
-
-# headers = {'Content-Type': 'application/json', 
-#     'Access-Control-Request-Headers': '*',
-#     'api-key': f'{api_key}'}
-
-# session = requests.Session()
-
-
-# session.headers.update(headers)
-
-
-# payload = {
-#     "dataSource": "Cluster1",
-#     "database": "Password_Manager",
-#     "collection": "sample_airbnb",
-#     "filter": {
-#         "name": "Ribeira Charming Duplex"
-#         # '_id': res['insertedId']
-#         # '_id': {'$oid': res['insertedId']}
-#     }
-# }
-
-
-# def find_one(session: requests.Session, payload: Dict[str, Any]) -> Dict[str, Any]:
-#     uri = f'{end_point}/find'
-#     result = session.post(uri, json=payload)
-#     return result
-
-
-# rec = find_one(session, payload)
-
-# print(rec.json())
-
-# r = rec.json()['document']
-
-# print(r)
-
-# r['name']
+        print(result.deleted_count)  # prints the number of deleted entries
+    except OperationFailure as ex:
+        print(ex)
+        raise ex
