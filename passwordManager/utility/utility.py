92beb1057ef81f77cf7d1a316ec09f8e3ea32b88
Richard DeGeus <crimson_eagle_26@hotmail.com>
2023-11-30 19:19:41 +0000

    updated utility

--- passwordManager/utility/utility.py
+++ passwordManager/utility/utility.py
@@ -8,22 +8,12 @@ from pymongo.errors import OperationFailure
 from typing import Any, Dict, List
 
 uri = 'mongodb+srv://cluster1.cjufb6h.mongodb.net/?authSource=%24external'  \
-<<<<<<< HEAD
-        '&authMechanism=MONGODB-X509&retryWrites=true&w=majority'
-
-path_to_certificate = 'pm_cert.pem'
-
-
-def create_connection() -> Any:
-
-=======
     '&authMechanism=MONGODB-X509&retryWrites=true&w=majority'
 
 path_to_certificate = 'utility/pm_cert.pem'
 
 
 def create_connection() -> int:
->>>>>>> issue/2
     """Creates connection to MongoDB database
 
     Raises:
@@ -38,22 +28,14 @@ def create_connection() -> int:
         db = client['testDB']
         collection = db['testCol']
         doc_count = collection.count_documents({})
-<<<<<<< HEAD
-        print(doc_count)  # Should print 0 as the testDB doesn't exist
-=======
         return doc_count  # Should print 0 as the testDB doesn't exist
->>>>>>> issue/2
     except errors.ConnectionFailure as ex:
         print(ex)
         raise ex
 
 
 def create_collection(database_name: str,
-<<<<<<< HEAD
-                      collection_name: str) -> None:
-=======
                       collection_name: str) -> str:
->>>>>>> issue/2
 
     """Creates a collection
 
@@ -72,22 +54,14 @@ def create_collection(database_name: str,
     try:
         db = client[database_name]
         db.create_collection(collection_name)
-<<<<<<< HEAD
-=======
         return db.list_collection_names()
->>>>>>> issue/2
     except OperationFailure as ex:
         print(ex)
         raise ex
 
 
 def insert_entry(database_name: str,
-<<<<<<< HEAD
-                 collection_name: str, entry: List[Any]) -> None:
-
-=======
                  collection_name: str, entry: Dict[str, Any]) -> None:
->>>>>>> issue/2
     """Inserts one {key: value} pair into collection
 
     Args:
@@ -107,12 +81,7 @@ def insert_entry(database_name: str,
     try:
         db = client[database_name]
         collection = db[collection_name]
-<<<<<<< HEAD
-        result = collection.insert_one(entry)
-        print(result.inserted_id)  # prints inserted _id
-=======
         collection.insert_one(entry)
->>>>>>> issue/2
     except OperationFailure as ex:
         print(ex)
         raise ex
@@ -120,10 +89,6 @@ def insert_entry(database_name: str,
 
 def insert_entries(database_name: str, collection_name: str,
                    entries: List[Any]) -> None:
-<<<<<<< HEAD
-
-=======
->>>>>>> issue/2
     """Inserts mutltiple {key: value} entries into collection as long as they
         are in a list
 
@@ -144,12 +109,7 @@ def insert_entries(database_name: str, collection_name: str,
     try:
         db = client[database_name]
         collection = db[collection_name]
-<<<<<<< HEAD
-        result = collection.insert_many(entries)
-        print(result.inserted_ids)  # prints inserted _ids
-=======
         collection.insert_many(entries)
->>>>>>> issue/2
     except OperationFailure as ex:
         print(ex)
         raise ex
@@ -157,10 +117,6 @@ def insert_entries(database_name: str, collection_name: str,
 
 def find_entries(database_name: str, collection_name: str,
                  entries: Dict[str, Any] | None = None) -> Any:
-<<<<<<< HEAD
-
-=======
->>>>>>> issue/2
     """Finds {key: value} listings in a collection
 
     Args:
@@ -206,10 +162,6 @@ def find_entries(database_name: str, collection_name: str,
 
 def update_entry(database_name: str, collection_name: str,
                  old_data: Dict[str, Any], new_data: Dict[str, Any]) -> None:
-<<<<<<< HEAD
-
-=======
->>>>>>> issue/2
     """Finds the first matching key of {key: value} filter and
         updates the value
 
@@ -233,13 +185,7 @@ def update_entry(database_name: str, collection_name: str,
     try:
         db = client[database_name]
         collection = db[collection_name]
-<<<<<<< HEAD
-        updated_data = collection.update_one(old_data, {"$set": new_data})
-        # prints the number of {key: value} affected
-        print(updated_data.modified_count)
-=======
         collection.update_one(old_data, {"$set": new_data})
->>>>>>> issue/2
     except OperationFailure as ex:
         print(ex)
         raise ex
@@ -247,10 +193,6 @@ def update_entry(database_name: str, collection_name: str,
 
 def update_entries(database_name: str, collection_name: str,
                    old_data: Dict[str, Any], new_data: Dict[str, Any]) -> None:
-<<<<<<< HEAD
-
-=======
->>>>>>> issue/2
     """Finds the all matching keys of {key: value} filter and updates the
         values
 
@@ -273,13 +215,7 @@ def update_entries(database_name: str, collection_name: str,
     try:
         db = client[database_name]
         collection = db[collection_name]
-<<<<<<< HEAD
-        updated_data = collection.update_many(old_data, {"$set": new_data})
-        # prints the number of {key: value} pairs affected
-        print(updated_data.modified_count)
-=======
         collection.update_many(old_data, {"$set": new_data})
->>>>>>> issue/2
     except OperationFailure as ex:
         print(ex)
         raise ex
@@ -287,10 +223,6 @@ def update_entries(database_name: str, collection_name: str,
 
 def delete_entry(database_name: str, collection_name: str,
                  old_data: Dict[str, Any]) -> None:
-<<<<<<< HEAD
-
-=======
->>>>>>> issue/2
     """Deletes the first matching {key: value} filter entry
 
     Args:
@@ -310,12 +242,7 @@ def delete_entry(database_name: str, collection_name: str,
     try:
         db = client[database_name]
         collection = db[collection_name]
-<<<<<<< HEAD
-        result = collection.delete_one(old_data)
-        print(result.deleted_count)  # prints the number of deleted entries
-=======
         collection.delete_one(old_data)
->>>>>>> issue/2
     except OperationFailure as ex:
         print(ex)
         raise ex
@@ -323,10 +250,6 @@ def delete_entry(database_name: str, collection_name: str,
 
 def delete_entries(database_name: str, collection_name: str,
                    old_data: Dict[str, Any]) -> None:
-<<<<<<< HEAD
-
-=======
->>>>>>> issue/2
     """Deletes entries matching {key: value} filter
 
     Args:
@@ -346,10 +269,6 @@ def delete_entries(database_name: str, collection_name: str,
     try:
         db = client[database_name]
         collection = db[collection_name]
-<<<<<<< HEAD
-        result = collection.delete_many(old_data)
-        print(result.deleted_count)  # prints the number of deleted entries
-=======
         collection.delete_many(old_data)
     except OperationFailure as ex:
         print(ex)
@@ -375,7 +294,6 @@ def delete_collection(database_name: str, collection_name: str) -> None:
         db = client[database_name]
         collection = db[collection_name]
         collection.drop()
->>>>>>> issue/2
     except OperationFailure as ex:
         print(ex)
         raise ex
